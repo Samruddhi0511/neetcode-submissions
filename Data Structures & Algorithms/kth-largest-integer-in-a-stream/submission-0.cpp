36b1dec1b8db44eef16dd17c121d120933d17133
Samruddhi Deshpande <167088134+Samruddhi0511@users.noreply.github.com>
2026-05-28 15:00:25 +0530

    Add: kth-largest-integer-in-a-stream - submission-0

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-0.cpp
@@ -0,0 +1,19 @@
+class KthLargest {
+    priority_queue<int, vector<int>, greater<int>> min_heap;
+        int size_limit;
+public:
+    KthLargest(int k, vector<int>& nums) {
+        size_limit=k;
+        for(int i : nums){
+            add(i);
+        }
+    }
+    
+    int add(int val) {
+        min_heap.push(val);
+        if(min_heap.size() > size_limit){
+            min_heap.pop();
+        }
+        return min_heap.top();
+    }
+};
